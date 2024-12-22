0f9bd02ba4883e060afef1cd838bee18b2f2781f
Mubasheer G <87425459+mubasheerg@users.noreply.github.com>
2024-12-22 14:57:56 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float versionoftype = 91.0f;
+		String developeroftype = "Google";
+		boolean isBetaoftype = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+
+		System.out.println("Versionoftype is " + versionoftype + "\n" + "Developeroftype is " + developeroftype + "\n"
+				+ "IsBetaoftype is " + isBetaoftype + "\n" + "ReleaseYear is " + releaseYear + "\n" + "ShortcutKey is "
+				+ shortcutKey);
+	}
+}
